7052ae587fed6eb773ad7f255636927106b8ba2b
jmuhire13 <jmuhire@alustudent.com>
2024-10-23 15:08:06 -0700

    07

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(10):  # Loop for the first digit
+    for j in range(i + 1, 10):  # Loop for the second digit, ensuring it's greater than the first
+        print("{:01d}{:01d}".format(i, j), end=", " if i < 8 else "\n")
