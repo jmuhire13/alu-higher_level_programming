df47abbd2d38701fef67a3d1030ba84a918a6be2
jmuhire13 <jmuhire@alustudent.com>
2024-10-23 16:16:42 -0700

    100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def print_tebahpla():
-    for i in range(26):
-        char = chr(122 - i) if i % 2 == 0 else chr(90 - i)  # 122 is 'z', 90 is 'Z'
-        print(char, end='')
+    print(''.join(chr(122 - i + (i % 2) * 32) for i in range(26)), end='')
 
 print_tebahpla()
