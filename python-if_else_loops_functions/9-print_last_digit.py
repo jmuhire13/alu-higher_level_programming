4e02e4cabc86615f62cc1a7783f9e7beec713336
jmuhire13 <jmuhire@alustudent.com>
2024-10-23 15:32:04 -0700

    10

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10  # Get the last digit using modulus
+    print(last_digit, end="")  # Print the last digit without a newline
+    return last_digit  # Return the value of the last digit
