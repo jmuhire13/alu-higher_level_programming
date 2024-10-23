e9e228ae28038b59c33eedad26e0cf5e71f362dd
jmuhire13 <jmuhire@alustudent.com>
2024-10-23 14:08:10 -0700

    04

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):  # ASCII values from 97 ('a') to 122 ('z')
-    if chr(i) not in ['e', 'q']:  # Skip 'e' and 'q'
-        print(chr(i), end="")  # Print each character without a new line
+for i in range(97, 123):  # ASCII range for lowercase letters
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end="")
