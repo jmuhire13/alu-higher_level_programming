a748366e3f87c18af38a30d8c6ae3c68a2486777
jmuhire13 <jmuhire@alustudent.com>
2024-10-23 16:23:07 -0700

    100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_tebahpla():
-    print("".join(chr(122 - i + (i % 2) * 32) for i in range(26)), end="")
+    for i in range(26):
+        print(chr(122 - i + (i % 2) * 32), end='')
 
 print_tebahpla()
