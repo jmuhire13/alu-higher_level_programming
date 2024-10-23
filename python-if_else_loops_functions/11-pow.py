eb2389da444e092d837cfc91194f93f3d1ccc9f8
jmuhire13 <jmuhire@alustudent.com>
2024-10-23 15:55:53 -0700

    12

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -5,10 +5,10 @@ def pow(a, b):
     if b == 0:
         return 1  # Any number to the power of 0 is 1
     elif b < 0:
-        result = 1
+        result = 1.0
         for _ in range(abs(b)):
             result /= a  # Use division for negative powers
-        return result
+        return float(f"{result:.15e}")  # Format the result in scientific notation
     else:
         result = 1
         for _ in range(b):
