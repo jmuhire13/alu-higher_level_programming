bff705ab7ceed968a4e8007f6acd48a73de7a7b2
jmuhire13 <jmuhire@alustudent.com>
2024-10-25 00:38:51 -0700

    00

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,4 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """Compute square values of all integers in a matrix."""
     return [[element ** 2 for element in row] for row in matrix]
