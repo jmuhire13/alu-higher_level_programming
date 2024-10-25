aeeb9e4d8d9ae8d9ed181a789d3897f2fd06010d
jmuhire13 <jmuhire@alustudent.com>
2024-10-25 02:15:39 -0700

    102

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,9 @@
 
 
 def complex_delete(a_dictionary, value):
-    keys_to_delete = [key for key in a_dictionary if a_dictionary[key] == value]
+    keys_to_delete = [
+        key for key in a_dictionary if a_dictionary[key] == value
+    ]
     for key in keys_to_delete:
         del a_dictionary[key]
     return a_dictionary
