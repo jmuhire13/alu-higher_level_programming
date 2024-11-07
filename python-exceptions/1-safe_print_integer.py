05708515d955e89c02528a4067c17395baf7ebba
jmuhire13 <j.muhire@alustudent.com>
2024-11-07 04:32:28 -0800

    1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
+def safe_print_integer(value):
     try:
-        for i in range(x):
-            print(my_list[i], end="")
-            count += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-        return count
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
 
 
 if __name__ == "__main__":
-    my_list = [1, "hello", 3, "world", 5]
-    result = safe_print_list(my_list, 7)
-    print("Number of elements printed:", result)
+    value = "hello"
+    result = safe_print_integer(value)
+    print("Is value printed as integer:", result)
